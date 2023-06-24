ea0e07915463a8198c4cd0b862914dcf2149e3c7
na0ki_mac <wehallo53.en@gmail.com>
2023-06-24 16:52:21 +0900

    all ok

--- app/src/main/java/com/example/whiteboard_sweeper/MainActivity.java
+++ app/src/main/java/com/example/whiteboard_sweeper/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.whiteboard_sweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity  extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener  {
+
+public class MainActivity  extends AppCompatActivity implements View.OnTouchListener,CompoundButton.OnCheckedChangeListener{
 
     public Button leftbutton;
     public Button leftturnbutton;
@@ -16,13 +21,12 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
     public Button rightturnbutton;
 
     public Button rightbutton;
+    public TextView textView_ForB;
     public String[][] UrlArray = {
-            {"ForwardLeft","TurnForwardRight", "Forward","TurnForwardLeft", "ForwardRight"},
-            {"BackLeft","TurnBackRight", "Back","TurnBackLeft", "BackRight"}
+            {"ForwardLeft","TurnForwardLeft", "Forward","TurnForwardRight", "ForwardRight"},
+            {"BackLeft","TurnBackLeft", "Back","TurnBackRight", "BackRight"}
     };
     public int num_forward_or_back=0;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,54 +37,65 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
         gobutton=(Button) findViewById(R.id.button_go);
         rightturnbutton=(Button) findViewById(R.id.button_right_turn);
         rightbutton=(Button) findViewById(R.id.button_right);
+        textView_ForB = (TextView) findViewById(R.id.textView_ForB);
 
-        leftbutton.setOnClickListener(this);
-        leftbutton.setOnLongClickListener(this);
-        leftturnbutton.setOnClickListener(this);
-        leftturnbutton.setOnLongClickListener(this);
-        gobutton.setOnClickListener(this);
-        gobutton.setOnLongClickListener(this);
-        rightturnbutton.setOnClickListener(this);
-        rightturnbutton.setOnLongClickListener(this);
-        rightbutton.setOnClickListener(this);
-        rightbutton.setOnLongClickListener(this);
+        leftbutton.setOnTouchListener(this);
+        leftturnbutton.setOnTouchListener(this);
+        gobutton.setOnTouchListener(this);
+        rightturnbutton.setOnTouchListener(this);
+        rightbutton.setOnTouchListener(this);
+
+        Switch switch_ForB = findViewById(R.id.switch_ForB);
+        switch_ForB.setOnCheckedChangeListener(this);
     }
+
+
     @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        String url_prm="click";
-        if (id == R.id.button_left) {
-            url_prm=UrlArray[num_forward_or_back][0];
-        } else if (id == R.id.button_go) {
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        Log.d("RaspiHttp","onTouch");
+        int id = view.getId();
+        String url_prm="touch";
+        if (motionEvent.getAction()==MotionEvent.ACTION_DOWN){//押した時
+            if (id == R.id.button_left) {//どのボタンなのかによってurlを決める
+                url_prm=UrlArray[num_forward_or_back][0];
+            } else if (id == R.id.button_left_turn) {
+                url_prm=UrlArray[num_forward_or_back][1];
+            } else if (id == R.id.button_go) {
+                url_prm=UrlArray[num_forward_or_back][2];
+            } else if (id == R.id.button_right_turn) {
+                url_prm=UrlArray[num_forward_or_back][3];
+            } else if (id == R.id.button_right) {
+                url_prm=UrlArray[num_forward_or_back][4];
+            } else {
 
-        } else if (id == R.id.button_right) {
+            }
+//            task.execute(1);
+        }
 
-        } else {
-
+        else {//離した時
+            url_prm="STOP";
         }
         RaspiHttp task=new RaspiHttp(this);
         task.execute(url_prm.toString());
+        return true;
     }
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Log.d("RaspiHttp","onCheckedChanged");
+        if (buttonView.getId() == R.id.switch_ForB) {
+            // Switch の状態が変更されたときの処理
+            if (isChecked) {
+                // Switch が ON の場合の処理
+                num_forward_or_back=1;
+                textView_ForB.setText("Back");
+            } else {
+                // Switch が OFF の場合の処理
+                num_forward_or_back=0;
+                textView_ForB.setText("Forward");
 
-    @Override
-    public boolean onLongClick(View v) {
-        int id = v.getId();
-        String url_prm="Long";
-        if (id == R.id.button_left) {
-            url_prm=UrlArray[num_forward_or_back][0];
-        } else if (id == R.id.button_left_turn) {
-            url_prm=UrlArray[num_forward_or_back][1];
-        } else if (id == R.id.button_go) {
-            url_prm=UrlArray[num_forward_or_back][2];
-        } else if (id == R.id.button_right_turn) {
-            url_prm=UrlArray[num_forward_or_back][3];
-        } else if (id == R.id.button_right) {
-            url_prm=UrlArray[num_forward_or_back][4];
-        } else {
-
+            }
         }
-        RaspiHttp task=new RaspiHttp(this);
-        task.execute(url_prm.toString());
-        return false;
+        Log.d("RaspiHttp","num_forward_or_back = " + num_forward_or_back);
+
     }
+
 }
