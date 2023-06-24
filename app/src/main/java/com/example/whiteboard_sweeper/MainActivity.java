a154ae228ace5f3344f2dbdf4331791c6c6412fc
na0ki_mac <wehallo53.en@gmail.com>
2023-06-24 10:30:54 +0900

    api

--- app/src/main/java/com/example/whiteboard_sweeper/MainActivity.java
+++ app/src/main/java/com/example/whiteboard_sweeper/MainActivity.java
@@ -3,12 +3,80 @@ package com.example.whiteboard_sweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity  extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener  {
+
+    public Button leftbutton;
+    public Button leftturnbutton;
+    public Button gobutton;
+
+    public Button rightturnbutton;
+
+    public Button rightbutton;
+    public String[][] UrlArray = {
+            {"ForwardLeft","TurnForwardRight", "Forward","TurnForwardLeft", "ForwardRight"},
+            {"BackLeft","TurnBackRight", "Back","TurnBackLeft", "BackRight"}
+    };
+    public int num_forward_or_back=0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        leftbutton=(Button) findViewById(R.id.button_left);
+        leftturnbutton=(Button) findViewById(R.id.button_left_turn);
+        gobutton=(Button) findViewById(R.id.button_go);
+        rightturnbutton=(Button) findViewById(R.id.button_right_turn);
+        rightbutton=(Button) findViewById(R.id.button_right);
+
+        leftbutton.setOnClickListener(this);
+        leftbutton.setOnLongClickListener(this);
+        leftturnbutton.setOnClickListener(this);
+        leftturnbutton.setOnLongClickListener(this);
+        gobutton.setOnClickListener(this);
+        gobutton.setOnLongClickListener(this);
+        rightturnbutton.setOnClickListener(this);
+        rightturnbutton.setOnLongClickListener(this);
+        rightbutton.setOnClickListener(this);
+        rightbutton.setOnLongClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        String url_prm="click";
+        if (id == R.id.button_left) {
+            url_prm=UrlArray[num_forward_or_back][0];
+        } else if (id == R.id.button_go) {
+
+        } else if (id == R.id.button_right) {
+
+        } else {
+
+        }
+        RaspiHttp task=new RaspiHttp(this);
+        task.execute(url_prm.toString());
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        int id = v.getId();
+        String url_prm="Long";
+        if (id == R.id.button_left) {
+            url_prm=UrlArray[num_forward_or_back][0];
+        } else if (id == R.id.button_go) {
+
+        } else if (id == R.id.button_right) {
+
+        } else {
+
+        }
+        RaspiHttp task=new RaspiHttp(this);
+        task.execute(url_prm.toString());
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/whiteboard_sweeper/RaspiHttp.java
@@ -0,0 +1,70 @@
+package com.example.whiteboard_sweeper;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
+
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RaspiHttp extends AsyncTask<String,Void,Void> {//ここが引数
+    private Activity parentActivity;
+    private ProgressDialog dialog=null;
+    private final String DEFAULTURL="http://192.168.11.36/~pi/ledtest.php?";
+    private String uri=null;
+    public RaspiHttp(Activity parentActivity){
+        this.parentActivity=parentActivity;
+    }
+    @Override
+    protected void onPreExecute(){
+        dialog=new ProgressDialog(parentActivity);
+        dialog.setMessage("通信中");
+        dialog.show();
+    }
+
+    @Override
+    protected  Void doInBackground(String...arg0){//ここが引数
+        uri=DEFAULTURL+"/"+arg0[0];
+        Log.d("RaspiHttp",uri);
+        exec_get();
+        return  null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result){
+        dialog.dismiss();
+    }
+
+    private String exec_get(){
+        HttpURLConnection http=null;
+        InputStream in=null;
+        String src=new String();
+        try{
+            URL url=new URL(uri);
+            http=(HttpURLConnection) url.openConnection();
+            http.setRequestMethod("GET");
+            http.connect();
+
+            in=http.getInputStream();
+            byte[] line=new byte[1024];
+            int size;
+            while (true){
+                size=in.read(line);
+                if(size<=0)break;
+                src+=new String(line);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(http!=null)http.disconnect();
+                if(in!=null)in.close();
+            }catch (Exception e){}
+        }
+        return  src;
+    }
+}
